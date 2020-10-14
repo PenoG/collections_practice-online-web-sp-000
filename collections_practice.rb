b5d2f4fb5d8c93a13ccab2fd3049390d6d1fd082
Mason Phillips <mjphillips2000@gmail.com>
2020-10-14 19:12:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,6 @@ def sort_array_char_count(char)
   char.sort{|x, y| x.length <=> y.length}
 end
 
-def swap_elements(char)
-  char[0]. char[2] = char[2], char[0]
+def swap_elements(chars)
+  chars[0]. chars[2] = chars[2], chars[0]
 end
